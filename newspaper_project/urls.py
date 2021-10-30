0cc21ffb684865023521d4728e15033ccc8d8539
rsancho64 <rsancho@iesrioarba.es>
2021-10-30 18:06:18 +0200

    .

--- newspaper_project/urls.py
+++ newspaper_project/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import include # new
+from django.views.generic.base import TemplateView # new
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')), # new
+    path('users/', include('django.contrib.auth.urls')), # new
+    path('', TemplateView.as_view(template_name='home.html'), name='home'), # new    
 ]
